d1475fe488ffe1d017d148bf49b1df024aa4488f
Denis Parashchenko <denus.parashencko@gmail.com>
2023-05-29 04:01:09 +0300

    Update
    Edit main.cpp

--- lab12/Project/main.cpp
+++ lab12/Project/main.cpp
@@ -1,44 +1,70 @@
 #include <iostream>
+#include <stdexcept>
+#include <Windows.h>
 
-using namespace std;
+class ClassLab12_Parashencko {
+private:
+  double radius;  // Радіус кола
 
-class classlab12_Parashencko {
-    private:
-        float radius;
-        float area;
+public:
+  // Конструктор класу
+  ClassLab12_Parashencko(double r) {
+    // Валідація вхідних даних
+    if (r < 0) {
+      throw std::invalid_argument("Радіус має бути додатнім числом");
+    }
+    radius = r;
+  }
 
-    public:
-        classlab12_Parashencko(float r) {
-            radius = r;
-            area = 3.14159 * r * r;
-        }
+  // Функція для зміни значення радіуса
+  void setRadius(double r) {
+    // Валідація вхідних даних
+    if (r < 0) {
+      throw std::invalid_argument("Радіус має бути додатнім числом");
+    }
+    radius = r;
+  }
 
-        float getRadius() {
-            return radius;
-        }
+  // Функція для отримання значення радіуса
+  double getRadius() const {
+    return radius;
+  }
 
-        float getArea() {
-            return area;
-        }
-
-        void setRadius(float r) {
-            radius = r;
-            area = 3.14159 * r * r;
-        }
+  // Функція для обчислення площі кола
+  double getArea() const {
+    return 3.14159 * radius * radius;
+  }
 };
 
+
+
 int main() {
-    classlab12_Parashencko circle(5);
+SetConsoleOutputCP(1251);
+SetConsoleCP(1251);
+//SetConsoleOutputCP(CP_UTF8);
+  double radius;
 
-    cout << "Initial values:" << endl;
-    cout << "Radius: " << circle.getRadius() << endl;
-    cout << "Area: " << circle.getArea() << endl;
+  std::cout << "Введіть радіус кола: ";
+  std::cin >> radius;
 
-    circle.setRadius(7);
+  try {
+    // Створення об'єкта класу ClassLab12_Parashencko
+    ClassLab12_Parashencko circle(radius);
 
-    cout << "New values:" << endl;
-    cout << "Radius: " << circle.getRadius() << endl;
-    cout << "Area: " << circle.getArea() << endl;
+    // Отримання значення радіуса та площі кола
+    std::cout << "Радіус кола: " << circle.getRadius() << std::endl;
+    std::cout << "Площа кола: " << circle.getArea() << std::endl;
 
-    return 0;
+    // Зміна значення радіуса та повторне обчислення площі кола
+    double newRadius;
+    std::cout << "Введіть нове значення радіуса: ";
+    std::cin >> newRadius;
+    circle.setRadius(newRadius);
+    std::cout << "Новий радіус кола: " << circle.getRadius() << std::endl;
+    std::cout << "Нова площа кола: " << circle.getArea() << std::endl;
+  } catch (const std::invalid_argument& e) {
+    std::cout << "Помилка: " << e.what() << std::endl;
+  }
+
+  return 0;
 }
